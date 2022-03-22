6eb32a473ee8c40cf6b38616cc29cc0e8fc74e17
Riya Dattani <riyaddattani@gmail.com>
2022-03-22 15:53:43 +0000

    add all combinations

--- go/gilded-rose_test.go
+++ go/gilded-rose_test.go
@@ -7,9 +7,9 @@ import (
 )
 
 func Test_Foo(t *testing.T) {
-	names := []string{"Aged Brie", "Backstage passes to a TAFKAL80ETC concert"}
-	sellins := []int{0, 1}
-	quality := []int{0, 1}
+	names := []string{"Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"}
+	sellins := []int{0, 1, 10, 11, 5, 6, -1}
+	quality := []int{0, 1, 49, 50, -1}
 
 	approvals.VerifyAllCombinationsFor3(t, "update quality", func(name, sellin, quality interface{}) string {
 		item := &Item{name.(string), sellin.(int), quality.(int)}
