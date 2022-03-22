9fb40a5b51f6017f467c17a2e013cec6f0b3c30d
Riya Dattani <riyaddattani@gmail.com>
2022-03-22 16:09:05 +0000

    100% coverage

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -9,29 +9,25 @@ type Item struct {
 
 type Items []*Item
 
-//
-func (i Items) String() string {
-	var s string
-	for _, item := range i {
-		s += fmt.Sprintf("%#v\n", item)
-	}
-
-	return s
-}
+const (
+	AgedBrie        = "Aged Brie"
+	BackstagePasses = "Backstage passes to a TAFKAL80ETC concert"
+	Sulfulars       = "Sulfuras, Hand of Ragnaros"
+)
 
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+		if items[i].name != AgedBrie && items[i].name != BackstagePasses {
 			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
+				if items[i].name != Sulfulars {
 					items[i].quality = items[i].quality - 1
 				}
 			}
 		} else {
 			if items[i].quality < 50 {
 				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+				if items[i].name == BackstagePasses {
 					if items[i].sellIn < 11 {
 						if items[i].quality < 50 {
 							items[i].quality = items[i].quality + 1
@@ -46,15 +42,15 @@ func UpdateQuality(items []*Item) {
 			}
 		}
 
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
+		if items[i].name != Sulfulars {
 			items[i].sellIn = items[i].sellIn - 1
 		}
 
 		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+			if items[i].name != AgedBrie {
+				if items[i].name != BackstagePasses {
 					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
+						if items[i].name != Sulfulars {
 							items[i].quality = items[i].quality - 1
 						}
 					}
@@ -69,4 +65,14 @@ func UpdateQuality(items []*Item) {
 		}
 	}
 
+}
+
+//
+func (i Items) String() string {
+	var s string
+	for _, item := range i {
+		s += fmt.Sprintf("%#v\n", item)
+	}
+
+	return s
 }
