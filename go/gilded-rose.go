2741fcca52f3c2a974a33354ec0ff1fc1005a74f
Riya Dattani <riyaddattani@gmail.com>
2022-03-22 16:18:28 +0000

    add some helper methods

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -7,6 +7,14 @@ type Item struct {
 	sellIn, quality int
 }
 
+func (i Item) lowQuality() bool {
+	return i.quality < 50
+}
+
+func (i Item) highQuality() bool {
+	return i.quality > 0
+}
+
 type Items []*Item
 
 const (
@@ -16,55 +24,54 @@ const (
 )
 
 func UpdateQuality(items []*Item) {
-	for i := 0; i < len(items); i++ {
-
-		if items[i].name != AgedBrie && items[i].name != BackstagePasses {
-			if items[i].quality > 0 {
-				if items[i].name != Sulfulars {
-					items[i].quality = items[i].quality - 1
+	for _, item := range items {
+		if item.name != AgedBrie && item.name != BackstagePasses {
+			if item.highQuality() {
+				if item.name != Sulfulars {
+					item.quality = item.quality - 1
 				}
 			}
 		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == BackstagePasses {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
+			if item.lowQuality() {
+				item.quality = item.quality + 1
+				if item.name == BackstagePasses {
+					if item.sellIn < 11 {
+						if item.lowQuality() {
+							item.quality = item.quality + 1
 						}
 					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
+					if item.sellIn < 6 {
+						if item.lowQuality() {
+							item.quality = item.quality + 1
 						}
 					}
 				}
 			}
 		}
 
-		if items[i].name != Sulfulars {
-			items[i].sellIn = items[i].sellIn - 1
+		if item.name != Sulfulars {
+			item.sellIn = item.sellIn - 1
 		}
 
-		if items[i].sellIn < 0 {
-			if items[i].name != AgedBrie {
-				if items[i].name != BackstagePasses {
-					if items[i].quality > 0 {
-						if items[i].name != Sulfulars {
-							items[i].quality = items[i].quality - 1
+		if item.sellIn < 0 {
+			if item.name != AgedBrie {
+				if item.name != BackstagePasses {
+					if item.highQuality() {
+						if item.name != Sulfulars {
+							item.quality = item.quality - 1
 						}
 					}
 				} else {
-					items[i].quality = items[i].quality - items[i].quality
+					item.quality = item.quality - item.quality
 				}
 			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
+				if item.lowQuality() {
+					item.quality = item.quality + 1
 				}
 			}
 		}
-	}
 
+	}
 }
 
 //
